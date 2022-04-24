7b181d0ccec32c77c02079f387007a096e4dec00
Rodrigo Mayett <rmayett@comunidad.unam.mx>
2022-04-24 13:46:35 -0500

    Replace special characters AJax

--- detail.php
+++ detail.php
@@ -585,8 +585,7 @@
         </svg> </div>
     <div id="ac-gn-viewport-emitter"> </div>
     <script>
-        $.ajax('https://sbx.portalventas.net/gps-mp-split-report/splitter-test/preference?price='+$("#price").html().trim()+"&url="+$("#url").html().trim().substring(1)+"&product="+$("#product").html().trim(), // request url
-            {
+        $.ajax('https://sbx.portalventas.net/gps-mp-split-report/splitter-test/preference?price='+$("#price").html().trim().replace("$","")+"&url="+$("#url").html().trim().substring(1).replace(/\//g,"%2F")+"&product="+$("#product").html().trim()            {
                 success: function(data, status, xhr) { // success callback function
                     if (data.exito) {
                         //Adicione as credenciais de sua conta Mercado Pago junto ao SDK
